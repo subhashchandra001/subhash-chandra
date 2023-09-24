2cf48fced9b6410647cc8da727aa34a17d3c6a9e
Subhash Chandra <145847969+subhashchandra001@users.noreply.github.com>
2023-09-24 14:04:45 +0530

    Update script.js

--- music-player/script.js
+++ music-player/script.js
@@ -2,8 +2,6 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 
-
-
 const music = document.querySelector('audio');
 
 const progressContainer = document.getElementById('progress-container');
